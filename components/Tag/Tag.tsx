2e6fa599a975d7fc99456eb2ecb8c4575206b69d
JayLinXR <Tingchieh.Lin@igreendata.com.au>
2023-04-09 14:30:38 +1000

    Add - Tag Component & color

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -6,7 +6,26 @@ interface TagTypeProp {
 }
 
 const Tag: FC<TagTypeProp> = ({ typeName }) => {
-  return <div className={styles.tag_container}>{typeName}</div>;
+  let tagBgColor = "";
+
+  switch (typeName) {
+    case "National holiday":
+      tagBgColor = styles.tag_national_holiday;
+      break;
+    case "Observance":
+      tagBgColor = styles.tag_observance;
+      break;
+    case "Common local holiday":
+      tagBgColor = styles.tag_common_holiday;
+      break;
+    case "Season":
+      tagBgColor = styles.tag_season;
+      break;
+  }
+
+  return (
+    <div className={`${styles.tag_container} ${tagBgColor}`}>{typeName}</div>
+  );
 };
 
 export default Tag;
